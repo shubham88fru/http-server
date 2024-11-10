08267792958eeefa798e8e5bf3e1a9c36f22d7ef
shubham88fru <shubham88fru@yahoo.com>
2024-11-10 16:59:45 -0700

    sample requests

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # http-server
 Http server
+
+- curl --request GET -v localhost:8081/something
+- curl --request GET -v localhost:8081/status
+- curl --request POST -v --data '50,100' localhost:8081/task
+- curl --request POST -v --header "X-Test: true"   --data '50,100' localhost:8081/task
+- curl --request POST -v --header "X-Debug: true"   --data '50,100' localhost:8081/task
---
+++ src/main/java/WebServer.java
@@ -0,0 +1,117 @@
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.math.BigInteger;
+import java.net.InetSocketAddress;
+import java.util.Arrays;
+import java.util.concurrent.Executors;
+
+public class WebServer {
+    private static final String TASK_ENDPOINT = "/task";
+    private static final String STATUS_ENDPOINT = "/status";
+
+    private final int port;
+    private HttpServer server;
+
+    public WebServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) {
+        int serverPort = 8080;
+        if (args.length == 1) {
+            serverPort = Integer.parseInt(args[0]);
+        }
+
+        WebServer webServer = new WebServer(serverPort);
+        webServer.startServer();
+        System.out.println("Server is listening of port " + serverPort);
+    }
+
+    public void startServer() {
+        //create an http server.
+        try {
+            //size of backlog is the number of requests we allow the server to keep
+            //in queue, if there aren't enough threads to take the request.
+            this.server = HttpServer.create(new InetSocketAddress(port), 0);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        HttpContext statusContext = server.createContext(STATUS_ENDPOINT); // `/status` endpoint.
+        statusContext.setHandler(this::handleStatusCheckRequest); //handler for /status endpoint.
+
+        HttpContext taskContext = server.createContext(TASK_ENDPOINT); // `/task` endpoint.
+        taskContext.setHandler(this::handleTaskRequest); //handler for /task endpoint.
+
+        server.setExecutor(Executors.newFixedThreadPool(8)); //thread pool for the server.
+        server.start();
+    }
+
+    //methods to handle the requests to endpoints.
+    // /status
+    private void handleStatusCheckRequest(HttpExchange exchange) throws IOException {
+        if (!exchange.getRequestMethod().equalsIgnoreCase("get")) {
+            exchange.close();
+            return;
+        }
+
+        String responseMessage = "Server is alive";
+        sendResponse(responseMessage.getBytes(), exchange);
+    }
+
+    private void handleTaskRequest(HttpExchange exchange) throws IOException {
+        if (!exchange.getRequestMethod().equalsIgnoreCase("post")) {
+            exchange.close();
+            return;
+        }
+
+        Headers headers = exchange.getRequestHeaders();
+        if (headers.containsKey("X-Test") && headers.get("X-Test").get(0).equalsIgnoreCase("true")) {
+            String dummyResponse = "123\n";
+            sendResponse(dummyResponse.getBytes(), exchange);
+            return;
+        }
+
+        boolean isDebugMode = false;
+        if (headers.containsKey("X-Debug") && headers.get("X-Debug").get(0).equalsIgnoreCase("true")) {
+            isDebugMode = true;
+        }
+
+        long startTime = System.nanoTime();
+        byte[] requestBytes = exchange.getRequestBody().readAllBytes();
+        byte[] responseBytes = calculateResponse(requestBytes);
+        long finishTime = System.nanoTime();
+
+        if (isDebugMode) {
+            String debugMessage = String.format("Operation took %d ns: ", finishTime - startTime);
+            exchange.getResponseHeaders().put("X-Debug-Info", Arrays.asList(debugMessage));
+        }
+        sendResponse(responseBytes, exchange);
+    }
+
+    private byte[] calculateResponse(byte[] requestBytes) {
+        String bodyString = new String(requestBytes);
+        String[] stringNumbers = bodyString.split(",");
+        BigInteger result = BigInteger.ONE;
+        for (String number: stringNumbers) {
+            BigInteger bigInteger = new BigInteger(number);
+            result = result.multiply(bigInteger);
+        }
+
+        return String.format("Result of multiplication is %s\n", result).getBytes();
+    }
+
+    private void sendResponse(byte[] responseBytes, HttpExchange exchange) throws IOException {
+        exchange.sendResponseHeaders(200, responseBytes.length);
+        OutputStream outputStream = exchange.getResponseBody();
+        outputStream.write(responseBytes);
+        outputStream.flush();
+        outputStream.close();
+    }
+}
